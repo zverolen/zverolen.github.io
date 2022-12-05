cb19d541de5b8fc3cb1bc8bd41b82327a87b8dbe
zverolen <zverolen@gmail.com>
2022-12-05 14:31:32 +0100

    Adds up to date code

--- source/js/script.js
+++ source/js/script.js
@@ -1,16 +1,43 @@
-const signupForm = document.getElementById('js-signupForm');
-const successMessage = document.getElementById('js-successMessage');
-const signupButton = document.getElementById('js-signupButton');
+// Complexity is needed because the sign up button event effects elements in different parts of the page
+//Note to self - it didn't start making particularly more sense though
+const signupElementsId = ['js-signupForm', 'js-successMessage', 'js-signupButton'];
+const ctaElementsId = ['js-ctaLink'];
+
+const signupElements = signupElementsId.map(elementId => {
+  return document.getElementById(elementId);
+});
+const ctaElements = ctaElementsId.map(elementId => {
+  return document.getElementById(elementId);
+});
+
+const signupButton = signupElements[2];
+const ctaLink = ctaElements[0];
+const successMessage = signupElements[1];
 const successMessageContent =  document.createElement('p');
-successMessageContent.textContent = 'You\'ve been added! Check up your email!';
+successMessageContent.textContent = 'You are added to the next Night Flight event! We sent you an email with all details.';
 successMessageContent.className = 'signup-successMessage';
 
-const toggleSignupView = function(event) {
-  event.preventDefault();
-  signupForm.toggleAttribute('hidden');
-  signupButton.toggleAttribute('hidden');
-  successMessage.toggleAttribute('hidden');
-  successMessage.appendChild(successMessageContent);
+
+const toggleVisibility = (elements) => {
+  elements.forEach((element) => {
+    element.toggleAttribute('hidden');
+  });
 };
 
-signupButton.addEventListener('click', toggleSignupView); 
+const hideAndShowElements = function(e) {
+  e.preventDefault();
+
+  toggleVisibility(signupElements);
+  toggleVisibility(ctaElements);
+
+  successMessage.appendChild(successMessageContent);  
+};
+
+signupButton.addEventListener('click', hideAndShowElements);
+
+// Going to form from CTA
+const firstFormIntput = document.querySelector('input');
+ctaLink.addEventListener('click', (e) => {
+  e.preventDefault();
+    firstFormIntput.focus();
+});
